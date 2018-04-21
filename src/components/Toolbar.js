5c0fd66cdb29a7c98ca011f7c72681e14b1277af
David Cheng <dc273@duke.edu>
2018-04-21 12:25:31 -0400

    SASS

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -13,8 +13,10 @@ export default class ToolBar extends React.Component {
     render() {
         return (
             <div className="bar__flex-container">
-                <button className="button button__add-sound"> Add Sound </button>
-                <button className="button button__play"> Play </button>
+                <div>
+                    <button className="button button__add-sound"> Add Sound </button>
+                    <button className="button button__play"> Play </button>
+                </div>
                 <button className="button button__add-files"> Add Files + </button>
             </div>  
         )
