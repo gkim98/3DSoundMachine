f7f0421975e40a175c1903a595a833c39cfb423a
David Cheng <dc273@duke.edu>
2018-04-21 11:46:59 -0400

    Work

---
+++ src/components/Bar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Howl, Howler } from 'howler';
+
+export default class Bar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+
+
+}
+
