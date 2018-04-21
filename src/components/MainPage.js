381b216497a5978194e4a1242fdf622524a4e18c
Grant Kim <grantkim93@gmail.com>
2018-04-21 11:44:21 -0400

    set up for main project

---
+++ src/components/MainPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Toolbar from './Toolbar'
+
+class MainPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                main page
+            </div>
+        )
+    }
+}
+
+export default MainPage
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import DavidPage from '../components/DavidPage';
 import GrantPage from '../components/GrantPage';
+import MainPage from '../components/MainPage';
 
 const AppRouter = () => (
     <BrowserRouter>
         <div>
             <Switch>
+                <Route path="/" component={MainPage} />
                 <Route path="/david" component={DavidPage} />
                 <Route path="/grant" component={GrantPage} />
             </Switch>
