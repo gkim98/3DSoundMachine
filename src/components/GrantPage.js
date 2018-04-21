53254569616ac834e080df94db55bf3c883de5be
Grant Kim <grantkim93@gmail.com>
2018-04-21 10:39:01 -0400

    sound configured

--- src/components/GrantPage.js
+++ src/components/GrantPage.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Howl } from 'howler';
+import birdSound from '../sounds/Budgie-sounds.mp3';
 
-const GrantPage = (props) => (
-    <div>
-        my page
-    </div>
-);
+const GrantPage = (props) => {
+
+    var sound = new Howl({
+        src: birdSound
+      });
+      
+      sound.play();
+
+    return (
+        <div>
+            my page
+        </div>
+    );
+};
 
 export default GrantPage;
