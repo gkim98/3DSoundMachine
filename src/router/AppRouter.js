55ac12cf06718329fee6e5fe1d52673feb514968
David Cheng <dc273@duke.edu>
2018-04-22 00:24:10 -0400

    Combine

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import DavidPage from '../components/DavidPage';
-import GrantPage from '../components/GrantPage';
 import MainPage from '../components/MainPage';
 
 const AppRouter = () => (
@@ -9,8 +7,6 @@ const AppRouter = () => (
         <div>
             <Switch>
                 <Route path="/" component={MainPage} exact={true} />
-                <Route path="/david" component={DavidPage} />
-                <Route path="/grant" component={GrantPage} />
             </Switch>
         </div>
     </BrowserRouter>
